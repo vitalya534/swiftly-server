73ab3472e3485985e95d0ad8dc2d027bd5e65e75
vitalya534 <voyko_2014@mail.ru>
2026-03-16 11:52:03 +0700

    fix port

--- index.js
+++ index.js
@@ -275,4 +275,5 @@ wss.on('connection', (ws) => {
   });
 });
 
-server.listen(3001, () => console.log('Server running on http://localhost:3001'));
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log('Server running on port ' + PORT));
